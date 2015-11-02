2efcf8f8ed216cf88f9ab00810519766e28333d7
Nathan <wagner.452@gmail.com>
2015-11-02 14:56:49 +0000

    more updates, tighten things up

--- radio_control.py
+++ radio_control.py
@@ -2,6 +2,8 @@
 import subprocess
 import RPi.GPIO as GPIO
 import pipes
+import time
+from array import *
 GPIO.setwarnings(False)
 cmd = ["/usr/local/bin/pianobar"]
 process = subprocess.Popen(cmd)
@@ -22,29 +24,35 @@ t=pipes.Template()
 ch3status=True
 ch2status=True
 combo1=False
+
+def write_ctl (commands_list):
+    for i in commands_list:
+        handle=t.open('/home/pi/.config/pianobar/ctl','w')
+        handle.write(i)
+        handle.close()
+
 def main_loop ():
     global ch3status
     global ch2status
+    global combo1
     input3 = GPIO.input(ch3)
     input2 = GPIO.input(ch2)
-    if input3 == False:
-        if ch3status != input3:
-            handle=t.open('/home/pi/.config/pianobar/ctl','w')
-            handle.write('p')
-            handle.close()
+    if input3 == False and ch3status != input3:
+        write_ctl("p")
     ch3status=input3
-    if input2 == False:
-        if ch2status != input2:
-            handle=t.open('/home/pi/.config/pianobar/ctl','w')
-            handle.write('n')
-            handle.close()
+    if input2 == False and ch2status != input2:
+        write_ctl("n")
     ch2status=input2
-    if ch2status==False:
-        if ch2status==ch3status:
-            handle=t.open('/home/pi/.config/pianobar/ctl','w')
-            handle.write('s')
-            handle.write('10')
-            handle.close()
+
+    if combo1==False:
+        if ch2status==False and ch3status==False:
+            combo1=True
+            write_ctl(["s10","\n"])
+            time.sleep(0.05)
+    ch2status=input2
+    ch3status=input3
+    if ch2status != ch3status
+        combo1=False
 
 while True:
     try:
